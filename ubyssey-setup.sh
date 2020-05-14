00a76b8a9b609d1c13ac982932fdc838a27bfba8
Keegan Landrigan <keeganland@gmail.com>
2020-05-14 14:22:08 -0700

    Make setup basic, more-than-minimal

--- ubyssey-setup.sh
+++ ubyssey-setup.sh
@@ -4,37 +4,29 @@
 # TODO make this conditional
 cp -r ubyssey.ca/_settings/settings-local.py ubyssey.ca/ubyssey/settings.py
 
-# in the original instructions this would be where we would build an run the docker container. The point of this file though is that we don't wanna do this
+# in the original instructions this would be where we would run 'docker-compose up'. The point of this setup file though is that we're running on the Docker container already.
 # so instead, we're going to ensure dependinces are installed
 #cd ubyssey.ca
-#apt-get update
-#apt-get -y install build-essential curl
-#apt-get install -y nodejs
-#apt-get install -y npm
-#pip install -r requirements.txt
+apt-get update
+apt-get -y install build-essential curl
+apt-get install -y nodejs
+apt-get install -y npm
 
 #set up the Ubyssey Theme's static directory
-#cd /ubyssey.ca/ubyssey/static/
-#npm install
-#npm install -g gulp
-#npm install -g gulp-cli
-#npm rebuild node-sass
-#gulp buildDev
-#cd ..
-#cd ..
-#cd ..
-#cd ..
+cd /ubyssey.ca/ubyssey/static/
+npm install
+npm install -g gulp
+npm install -g gulp-cli
+npm rebuild node-sass
+gulp buildDev
 
 #set up dispatch
-#cd dispatch
-#pip install -e .[dev]
-#python setup.py develop
-#cd dispatch/static/manager
-#npm install -g yarn
-#yarn setup
-#cd ..
-#cd ..
-#cd ..
-#cd ..
-#cd ubyssey.ca
+cd /home/dispatch
+pip install -e .[dev]
+python setup.py develop
+cd /home/dispatch/static/manager
+npm install -g yarn
+yarn setup
+
+#start server
 python /home/ubyssey.ca/manage.py runserver 0.0.0.0:8000
