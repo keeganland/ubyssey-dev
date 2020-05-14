96fbf9bd12a7af79a238c28089ef1537aef97642
Keegan Landrigan <keeganland@gmail.com>
2020-05-14 14:58:28 -0700

    Update setup shell to try to run commands that were previously done by running entire new (persisting!) containers

--- ubyssey-setup.sh
+++ ubyssey-setup.sh
@@ -13,20 +13,26 @@ apt-get install -y nodejs
 apt-get install -y npm
 
 #set up the Ubyssey Theme's static directory
+#the "yarn install" and "gulp" commands were being done in another container before, for some reason
 cd /ubyssey.ca/ubyssey/static/
 npm install
 npm install -g gulp
 npm install -g gulp-cli
 npm rebuild node-sass
 gulp buildDev
+yarn install
+gulp
 
 #set up dispatch
+#the "yarn install" and "yarn start" commands were being done by starting another container before, for some reason
 cd /home/dispatch
 pip install -e .[dev]
 python setup.py develop
-cd /home/dispatch/static/manager
+cd /home/dispatch/dispatch/static/manager
 npm install -g yarn
 yarn setup
+yarn install
+yarn start
 
 #start server
 python /home/ubyssey.ca/manage.py runserver 0.0.0.0:8000
