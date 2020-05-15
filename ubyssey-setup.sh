ed1c4f9b2bba9da7ffcb8f104a0ceee1fce08c94
Keegan Landrigan <keeganland@gmail.com>
2020-05-15 13:26:36 -0700

    remove unnecessary and slow installs from setup script

--- ubyssey-setup.sh
+++ ubyssey-setup.sh
@@ -4,14 +4,6 @@
 # TODO make this conditional
 cp -r ubyssey.ca/_settings/settings-local.py ubyssey.ca/ubyssey/settings.py
 
-# in the original instructions this would be where we would run 'docker-compose up'. The point of this setup file though is that we're running on the Docker container already.
-# so instead, we're going to ensure dependinces are installed
-#cd ubyssey.ca
-apt-get update
-apt-get -y install build-essential curl
-apt-get install -y nodejs
-apt-get install -y npm
-
 #set up the Ubyssey Theme's static directory
 #the "yarn install" and "gulp" commands were being done in another container before, for some reason
 cd /home/ubyssey.ca/ubyssey/static/
@@ -20,8 +12,6 @@ npm install -g gulp
 npm install -g gulp-cli
 npm rebuild node-sass
 gulp buildDev
-#yarn install
-#gulp
 
 #set up dispatch
 #the "yarn install" and "yarn start" commands were being done by starting another container before, for some reason
@@ -31,8 +21,6 @@ python setup.py develop
 cd /home/dispatch/dispatch/static/manager
 npm install -g yarn
 yarn setup
-#yarn install
-#yarn start
 
 #start server
 python /home/ubyssey.ca/manage.py runserver 0.0.0.0:8000
