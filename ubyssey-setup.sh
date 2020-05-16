715e565450bb2516cc543def76a554f76bf08c69
Keegan Landrigan <keeganland@gmail.com>
2020-05-15 17:31:48 -0700

    fix absolute paths in ubyssey-setup.sh

--- ubyssey-setup.sh
+++ ubyssey-setup.sh
@@ -2,7 +2,7 @@
 
 # tell Django to use the settings-local file for 
 # TODO make this conditional
-cp -r /workspaces/ubyssey-dev/ubyssey.ca/_settings/settings-local.py /workspaces/ubyssey-dev/ubyssey.ca/ubyssey/settings.py
+cp -r /workspaces/ubyssey.ca/_settings/settings-local.py /workspaces/ubyssey.ca/ubyssey/settings.py
 
 apt-get update
 apt-get install -y build-essential curl
@@ -11,7 +11,7 @@ apt-get install -y npm
 
 #set up the Ubyssey Theme's static directory
 #the "yarn install" and "gulp" commands were being done in another container before, for some reason
-cd /workspaces/ubyssey-dev/ubyssey.ca/ubyssey/static/
+cd /workspaces/ubyssey.ca/ubyssey/static/
 npm install
 npm install -g gulp
 npm install -g gulp-cli
@@ -20,9 +20,9 @@ gulp buildDev
 
 #set up dispatch
 #the "yarn install" and "yarn start" commands were being done by starting another container before, for some reason
-cd /workspaces/ubyssey-dev/dispatch
+cd /workspaces/dispatch
 pip install -e .[dev]
 python setup.py develop
-cd /workspaces/ubyssey-dev/dispatch/dispatch/static/manager
+cd /workspaces/dispatch/dispatch/static/manager
 npm install -g yarn
 yarn setup
